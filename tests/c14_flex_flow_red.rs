06632d99328a034d22033e9d4c8ca43f059730c2
bj-data-eng <larks-embassy.5j@icloud.com>
2026-08-14 03:19:20 -0400

    Revert "test: correct C14 flex-flow RED"
    This reverts commit 665ba51710956b8945d8d67b3c389d9e01504236.

--- tests/c14_flex_flow_red.rs
+++
@@ -1,9 +0,0 @@
-use surgeist_css::parse_style_attribute;
-
-#[test]
-fn valid_authored_flex_flow_is_retained_without_recovery() {
-    let report = parse_style_attribute("flex-flow: column wrap");
-
-    assert!(report.is_clean(), "{:?}", report.diagnostics());
-    assert_eq!(report.syntax().len(), 1);
-}
